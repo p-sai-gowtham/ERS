6a03e24f88670eab2098541c9bac694625db999d
gowkar69 <tennetisrikar@gmail.com>
2024-02-17 01:56:48 +0530

    add counter

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at the home index.")
+    return render(request, 'counter.html')
