35508bc08133ab9684375f74b93aa503ec86a9b6
p-sai-gowtham <puvvulasaigowtham@gmail.com>
2024-02-17 02:43:13 +0530

    add counter

--- ERS/settings.py
+++ ERS/settings.py
@@ -112,12 +112,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
